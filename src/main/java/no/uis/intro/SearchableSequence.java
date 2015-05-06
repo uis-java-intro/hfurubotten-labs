b274475f6fab3ebebd31fb493197c99161fb47d8
Heine Furubotten <hfurubotten@gmail.com>
2015-05-06 13:17:36 +0200

    finished the assignment

--- src/main/java/no/uis/intro/SearchableSequence.java
+++ src/main/java/no/uis/intro/SearchableSequence.java
@@ -1,5 +1,5 @@
 package main.java.no.uis.intro;
 
-public interface SearchableSequence {
-  
+public interface SearchableSequence extends Sequence {
+  public int findElementIn(int k, int limit);
 }
